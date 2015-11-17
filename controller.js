dd0e611f6ee1f671721673aeae134844fc9d56fe
yyamakawa41 <yyamakawa@gmail.com>
2015-11-17 14:02:53 -0500

    link to other country stats
    angular pulls from object

--- controller.js
+++ controller.js
@@ -5,6 +5,10 @@ countryApp.config(function($routeProvider){
 		templateUrl: 'country-list.html',
 		controller: 'countryCntrl'
 	}).
+	when('/:countryName/:population',{
+		templateUrl: 'country-detail.html',
+		controller: 'countryDetailCntrl'
+	}).
 	when('/:countryName',{
 		templateUrl: 'country-detail.html',
 		controller: 'countryDetailCntrl'
@@ -21,6 +25,14 @@ countryApp.controller('countryCntrl', function($scope, $http){
 });
 
 
-countryApp.controller('countryDetailCntrl', function($scope, $routeParams){
+countryApp.controller('countryDetailCntrl', function($scope, $routeParams, $http){
 	$scope.name = $routeParams.countryName;
+	$scope.population = $routeParams.population;
+	$http.get('countries.json').success(function(countryDetailData){
+		var country = countryDetailData.filter(function(currCountry){
+			return currCountry.name === $scope.name;
+		})[0];
+		$scope.country = country;
+		
+	});
 });
