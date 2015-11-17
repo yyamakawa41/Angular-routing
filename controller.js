f299bafddf466c987ca24af70d39f84b3c8dfe5e
yyamakawa41 <yyamakawa@gmail.com>
2015-11-17 11:52:37 -0500

    Extract template
    Extract template from HTML files

--- controller.js
+++ controller.js
@@ -1,7 +1,26 @@
-var countryApp = angular.module('countryApp', []);
+var countryApp = angular.module('countryApp', ['ngRoute']);
+
+countryApp.config(function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl: 'country-list.html',
+		controller: 'countryCntrl'
+	}).
+	when('/:countryName',{
+		templateUrl: 'country-detail.html',
+		controller: 'countryDetailCntrl'
+	}).
+	otherwise({
+		redirectTo: '/'
+	});
+});
 
 countryApp.controller('countryCntrl', function($scope, $http){
 	$http.get('countries.json').success(function(countryData){
 		$scope.countries = countryData;
 	});
+});
+
+
+countryApp.controller('countryDetailCntrl', function($scope, $routeParams){
+	console.log($routeParams)
 });
